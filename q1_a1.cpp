8a8ead68fa1932807d1fbbe5edfe7e878d5ad6d9
MOHIT20021 <mohit.maurya@iitgn.ac.in>
2023-09-28 16:53:48 +0530

    final commit of a1.c

---
+++ q1_a1.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+void print_a(int a[],int n){
+    for (int i=0;i<n;i++){
+        cout<<a[i]<<" "<<endl;
+    }
+}
+void com(int a[],int n , int k , int buf[],int idx){
+    
+    buf[c]=a[c];
+    for (int i = c+1;i<k;i++){
+        buf
+
+    }
+
+} 
+
+
+int main(){
+
+
+
+
+    return 0;
+}
